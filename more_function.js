75ab3ee25c112a06782ecfe01d1e69addeac4ed0
Nathaniel Berhane <Nathanielberhane9@gmail.com>
2024-04-30 09:10:16 -0400

    first commit

---
+++ more_function.js
@@ -0,0 +1,4 @@
+function displayMailingLabel(name, address,city, state, zip) {
+    console.log(name, address, city, state, zip)
+}
+displayMailingLabel("Nathaniel", "Rutland Road", "Brooklyn, New York, 11212")
