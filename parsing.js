7ec7964f58bd0811c7156d79692af2ebb6414a97
Nathaniel Berhane <Nathanielberhane9@gmail.com>
2024-05-01 13:22:10 -0400

    parsing temple

---
+++ parsing.js
@@ -0,0 +1,15 @@
+"use strict"
+let name = "Nathaniel Berhane";
+let positionOfSpace = name.indexOf(" ");
+console.log(positionOfSpace);
+
+let FirstName = name.substring(0, positionOfSpace0);
+console.log(firstName);
+
+let lastName = name.subsubstring(positionOfSpace +1);
+console.log(lastName);
+
+let message = "Your first name is $(firstName) and your last name is $lastName)."
+console.log(message)
+
+
