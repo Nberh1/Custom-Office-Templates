1a8fba96419c4c12a5bb8749c4e1663ef42008a3
Nathaniel Berhane <Nathanielberhane9@gmail.com>
2024-05-01 14:46:35 -0400

    more function version two

--- more_function.js
+++ more_function.js
@@ -1,5 +1,13 @@
 "use strict"
 function displayMailingLabel(name, address,city, state, zip) {
-    console.log(name, address, city, state, zip)
+    // console.log(name, address, city, state, zip);
+    console.log(name)
+    console.log(address)
+    console.log(city + ",", state, zip)
 }
 displayMailingLabel("Nathaniel", "Rutland Road", "Brooklyn, New York, 11212")
+displayMailingLabel("Brain", "estepl")
+
+
+displayHailingLable(nameUser1,address1,city1,state1,zip1)
+
