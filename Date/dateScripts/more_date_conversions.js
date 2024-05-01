d7446af00331f2c638446d41713411f17b0c30ec
Nathaniel Berhane <Nathanielberhane9@gmail.com>
2024-05-01 14:45:29 -0400

    date examples

---
+++ Date/dateScripts/more_date_conversions.js
@@ -0,0 +1,14 @@
+"use strict"
+let birthday = new Date(2000, 10, 17);
+console.log(birthday.toLocaleString());
+
+
+let momsBirthday = new Date(1979, 2, 21);
+console.log(momsBirthday.toLocaleString());
+
+
+let dadsBirthday = new Date(1975, 8, 2);
+console.log(dadsBirthday.toLocaleString());
+
+
+
